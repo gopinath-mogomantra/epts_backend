d291e943d06b07a638575a2d5382f2cb81e69bed
gopinath-mogomantra <gopinath.chennu@mogomantra.co.in>
2025-10-13 20:56:49 +0530

    Add render_migrate script to run migrations and collectstatic on Render

--- epts_backend/settings.py
+++ epts_backend/settings.py
@@ -12,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------------------------------------------
 SECRET_KEY = 'django-insecure-very-strong-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['epts-backend.onrender.com', '127.0.0.1', 'localhost']
+
 
 # -------------------------------------------------------------------
 # APPLICATION DEFINITION
@@ -79,7 +80,9 @@ WSGI_APPLICATION = 'epts_backend.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.config(
+        default='mysql://epts_user:Mogo@12345@106.222.230.233:3306/mogomantraepts',
+    )
 }
 
 
